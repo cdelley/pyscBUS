ce7c3802deb5d5961f2f5d03eab2eb1e86b431d6
cdelley <cyrille.delley@ucsf.edu>
2024-01-08 22:42:55 +0100

    added binary file format compatible with earlier work from myself. Additionally, added __init__ for submodule purposes.

--- BUSlib.py
+++ BUSlib.py
@@ -19,10 +19,11 @@ import math
 import struct
 from typing import List, Dict, Tuple, Union, BinaryIO
 import warnings
+from xopen import xopen
 
 BUSFORMAT_VERSION = 1
 
-def stringToBinary(s: str, length: int = 32, formating: str = '064b')  -> int:
+def stringToBinary(s: str, length: int = 32, formatting: str = '064b')  -> int:
     """
     Converts DNA string to binary number representation with:
     A   -> 00
@@ -33,10 +34,10 @@ def stringToBinary(s: str, length: int = 32, formating: str = '064b')  -> int:
     Input
         s: DNA string
         length: length of s, if not provided uses len(s) (default None)
-        formating: binary format of the output, (default '032b')
+        formatting: binary format of the output, (default '032b')
     
     Returns
-        int (formatted accoding to 'formating')
+        int (formatted accoding to 'formatting')
     """
     bs = s[:length].replace(
         'A','00').replace(
@@ -44,7 +45,7 @@ def stringToBinary(s: str, length: int = 32, formating: str = '064b')  -> int:
         'G','10').replace(
         'T','11')
     r = int(bs, 2)
-    #return format(r, formating)
+    #return format(r, formatting)
     return int(r)
     
 def binaryToString(x: int, length: int)  -> str:
@@ -75,16 +76,25 @@ def binaryToString(x: int, length: int)  -> str:
     return "".join(s)
 
 class BUSFIle(object):
-    def __init__(self):
-        self.text = ""
-        self.bcd = dict()  # type: dict[BC : [(UMI1, ecs1, count, flag), 
-                          #                  (UMI2, ecs1, count, flag), 
-                          #                  (UMI1, ecs2, count, flag), ....]
-        self.version = 0
-        self.bclen = 0
-        self.umilen = 0
-        self.textlen = 0
-        self.n_entries = 0
+
+    def __init__(
+        self,
+        text = "",
+        bcd = dict(),
+        bclen = 0,
+        umilen = 0,
+        textlen = 0,
+        n_entries = 0
+    ):
+        self.text = text
+        self.bcd = bcd     # type: dict[BC : [(UMI1, ecs1, count, flag), 
+                           #                  (UMI2, ecs1, count, flag), 
+                           #                  (UMI1, ecs2, count, flag), ....]
+        self.version = BUSFORMAT_VERSION
+        self.bclen = bclen
+        self.umilen = umilen
+        self.textlen = textlen
+        self.n_entries = n_entries
         self.format = ''
     
     @classmethod
@@ -94,10 +104,12 @@ class BUSFIle(object):
         """
         instance = cls()
         instance.in_file_path = in_file
-        with open(in_file, 'rb') as inf:
+        with xopen(in_file, 'rb', threads=2, compresslevel=3) as inf:
             instance.parseHeader(inf)
             if instance.format == 'BUS':
                 instance.parseBUS(inf)
+            elif instance.format == 'CBIN':
+                instance.parseCBIN(inf)
             else:
                 print('file format {} not implemented'.format(instance.format))
         return instance
@@ -113,9 +125,12 @@ class BUSFIle(object):
             b = inf.read(32)
             self.n_entries += 1
             try:
-                self.bcd[bc].append((umi, ec, num, flag))
+                self.bcd[bc][ec].append((umi, num, flag))
             except KeyError:
-                self.bcd[bc] = [(umi, ec, num, flag)] 
+                try:
+                    self.bcd[bc][ec] = [(umi, num, flag)]
+                except KeyError:
+                    self.bcd[bc] = {ec : [(umi, num, flag)]}
     
     def BUS_to_txt(self) -> None:
         # binaryToString(bc, self.bclen), binaryToString(umi, self.umilen), ec, num
@@ -128,6 +143,8 @@ class BUSFIle(object):
         magic = struct.unpack("<4s", inf.read(4))[0]
         if magic == b"BUS\x00":
             self.format = 'BUS'
+        elif magic == b"CBIN":
+            self.format = magic.decode('utf-8')
         else:
             self.format = magic    
             return       
@@ -136,17 +153,19 @@ class BUSFIle(object):
             warnings.warn("The BUS file version is different from the PyBUS decoder version")
         self.text = struct.unpack("<{}s".format(self.textlen), inf.read(self.textlen))[0]
     
-    def write(self, outf: str, formating: str = 'BUS') -> None:
+    def write(self, outf: str, formatting: str = 'BUS') -> None:
         """
         write an uncompressed BUS file
         """
         self.out_file_path = outf
-        with open(outf, 'wb') as fout:
-            self.writeHeader(fout)
-            if formating == 'BUS':
+        with xopen(outf, 'wb', threads=2, compresslevel=3) as fout:
+            self.writeHeader(fout, formatting)
+            if formatting == 'BUS':
                 self.writeBUS(fout)
+            elif formatting == 'CBIN':
+                self.writeCBIN(fout)
     
-    def writeHeader(self, outf: BinaryIO) -> None:
+    def writeHeader(self, outf: BinaryIO, formatting: str) -> None:
         """
         write the header of an uncompressed BUS file
         """
@@ -154,7 +173,7 @@ class BUSFIle(object):
             text_bytes = self.text.encode()
         except AttributeError:
             text_bytes = self.text
-        outf.write(b"BUS\x00")
+        outf.write(struct.pack("<4s", formatting.encode()))
         outf.write(struct.pack('<IIII', self.version, self.bclen, self.umilen, len(text_bytes)))
         outf.write(text_bytes)
         
@@ -163,9 +182,42 @@ class BUSFIle(object):
         write the data blocks of an uncompressed BUS file
         """
         if not sort_key:
-            for bc, value in self.bcd.items():
-                for tup in value:
-                    block = struct.pack('<QQIIII', bc, tup[0], tup[1], tup[2], tup[3], 0)
-                    outf.write(block)
-            
+            for bc, ec_dict in self.bcd.items():
+                for ec, value in ec_dict.items():
+                    for tup in value:
+                        block = struct.pack('<QQIIII', bc, tup[0], ec, tup[1], tup[2], 0)
+                        outf.write(block)
 
+    def writeCBIN(self, outf: BinaryIO, sort_key: str = None) -> None:
+        """
+        write the data blocks formated in cy binary
+        """
+        if not sort_key:
+            bc0 = -1
+            for bc, ec_dict in self.bcd.items():
+                block1 = struct.pack('<QI', bc, len(ec_dict))
+                outf.write(block1)
+                for ec, value in ec_dict.items():
+                    block2 = struct.pack('<II', ec, len(value))
+                    outf.write(block2)
+                    for tup in value:
+                        block3 = struct.pack('<QI', tup[0], tup[1])
+                        outf.write(block3)
+                    
+    def parseCBIN(self, inf: BinaryIO) -> None:
+        """
+        parse the data blocks of a cy binary file
+        """
+        b = inf.read(12)
+        while b:
+            bc, n_ec = struct.unpack('<QI', b)
+            self.bcd[bc] = dict()
+            for i in range(n_ec):
+                e = inf.read(8)
+                ec, n_umi = struct.unpack('<II', e)
+                u = inf.read(int(n_umi*12))
+                umi_c = iter(struct.unpack('<'+'QI'*n_umi, u))
+                self.bcd[bc][ec] = list(zip(umi_c, umi_c))
+            b = inf.read(12)
+            self.n_entries += 1
+                    
